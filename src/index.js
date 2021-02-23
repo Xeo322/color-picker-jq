8e7459c416636fa1eb471d51865d2f82b69173d8
Xeo322 <varg.vikernes111@gmail.com>
2021-02-23 06:53:40 +0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './styles/main.scss'
 
 $(() => {
-    let item = "#swatch"
+    let item = '#swatch'
 
     $('.btn').click((e) => {
         $('.btn').removeClass('active')
@@ -9,10 +9,6 @@ $(() => {
         item = e.target.value
     })
 
-    const changeItem = (value) => {
-        item = value
-        console.log(item)
-    }
     const hexFromRGB = (r, g, b) => {
         let hex = [
             r.toString(16),
@@ -21,28 +17,28 @@ $(() => {
         ];
         $.each(hex, (nr, val) => {
             if (val.length === 1) {
-                hex[nr] = "0" + val
+                hex[nr] = '0' + val
             }
         });
-        return hex.join("").toUpperCase()
+        return hex.join('').toUpperCase()
     }
     const refreshSwatch = () => {
-        let red = $("#red").slider("value"),
-            green = $("#green").slider("value"),
-            blue = $("#blue").slider("value"),
+        let red = $("#red").slider('value'),
+            green = $("#green").slider('value'),
+            blue = $("#blue").slider('value'),
             hex = hexFromRGB(red, green, blue)
-        $(item).css((item === '#swatch' ? "background-color" : 'color'), "#" + hex)
+        $(item).css((item === '#swatch' ? 'background-color' : 'color'), '#' + hex)
     }
 
-    $("#red, #green, #blue").slider({
-        orientation: "horizontal",
-        range: "min",
+    $('#red, #green, #blue').slider({
+        orientation: 'horizontal',
+        range: 'min',
         max: 255,
         value: 127,
         slide: refreshSwatch,
         change: refreshSwatch
-    });
-    $("#red").slider("value", 255);
-    $("#green").slider("value", 140);
-    $("#blue").slider("value", 60);
-});
+    })
+    $('#red').slider('value', 255)
+    $('#green').slider('value', 140)
+    $('#blue').slider('value', 60)
+})
